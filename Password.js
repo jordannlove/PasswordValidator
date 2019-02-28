d6428cd8a7f3803ce6f72e070af3adc9ac58ad2d
Jordan Love <jnlove314@gmail.com>
2019-02-28 13:24:38 -0600

    Add js file, add validation method

---
+++ Password.js
@@ -0,0 +1,15 @@
+function isValid()
+{
+  let pass1 = document.getElementById("password1").value;
+  let pass2 = document.getElementById("password2").value;
+  console.log(pass1);
+  console.log(pass2);
+  if(pass1 == pass2)
+  {
+    console.log("Valid");
+  }
+  else
+  {
+    console.log("Invalid");
+  }
+}
